ebc50e7980f6685e3611f4a753bd429a87564eee
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-10-12 22:12:31 +0530

    Formated App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "./components/Home"
-import Paste from "./components/Paste"
-import ViewPaste from "./components/ViewPaste"
-import Navbar from "./components/Navbar"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Paste from "./components/Paste";
+import ViewPaste from "./components/ViewPaste";
+import Navbar from "./components/Navbar";
 
-
-const router = createBrowserRouter(
-  [
-    {
-      path:"/",
-      element:
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
       <div className="w-full h-full flex flex-col">
-        <Navbar/>
-        <Home/>
+        <Navbar />
+        <Home />
       </div>
-    },
-    {
-      path:"/pastes",
-      element: <div className="w-full h-full flex flex-col">
-      <Navbar/>
-      <Paste/>
-    </div>
-    },
-    {
-      path:"/pastes/:id",
-      element: <div className="w-full h-full flex flex-col">
-      <Navbar/>
-      <ViewPaste/>
-    </div>,
-    }
-  ]
-)
+    ),
+  },
+  {
+    path: "/pastes",
+    element: (
+      <div className="w-full h-full flex flex-col">
+        <Navbar />
+        <Paste />
+      </div>
+    ),
+  },
+  {
+    path: "/pastes/:id",
+    element: (
+      <div className="w-full h-full flex flex-col">
+        <Navbar />
+        <ViewPaste />
+      </div>
+    ),
+  },
+]);
 
 function App() {
-
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
